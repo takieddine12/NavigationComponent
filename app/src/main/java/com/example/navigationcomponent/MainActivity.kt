ebf70294ec8ac2d1a20ee19c9fc9b4e90bfa4bc5
takieddine12 <algeriahero22@gmail.com>
2020-06-09 20:44:18 +0100

    Navigation Component with Drawer Navigation

--- app/src/main/java/com/example/navigationcomponent/MainActivity.kt
+++ app/src/main/java/com/example/navigationcomponent/MainActivity.kt
@@ -3,8 +3,13 @@ package com.example.navigationcomponent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
 import androidx.navigation.ActivityNavigatorExtras
+import androidx.navigation.NavGraph
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupWithNavController
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -13,12 +18,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        nav.setupWithNavController(nav_host_fragment_container.findNavController())
+       nav.setupWithNavController(nav_host_fragment_container.findNavController())
+        var navController = findNavController(R.id.nav_host_fragment_container)
+        var appConfiguration = AppBarConfiguration(navController.graph)
+        toolbar.setupWithNavController(navController,appConfiguration)
 
-        setSupportActionBar(toolbar)
         var actionBarDrawerToggle = ActionBarDrawerToggle(this,drawer,toolbar,0,0)
         actionBarDrawerToggle.syncState()
         drawer.addDrawerListener(actionBarDrawerToggle)
-
+        
     }
 }
