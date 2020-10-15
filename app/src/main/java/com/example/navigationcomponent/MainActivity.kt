7a41a77a252eb902f9a76d3ce846319ac6b1785f
Mehul-Bisht <mehulbishtanna@gmail.com>
2020-10-16 02:58:35 +0530

    bug fix

--- app/src/main/java/com/example/navigationcomponent/MainActivity.kt
+++ app/src/main/java/com/example/navigationcomponent/MainActivity.kt
@@ -2,30 +2,59 @@ package com.example.navigationcomponent
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.widget.Toolbar
-import androidx.navigation.ActivityNavigatorExtras
-import androidx.navigation.NavGraph
-import androidx.navigation.findNavController
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.*
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.example.navigationcomponent.fragments.homeFragmentDirections
+import com.example.navigationcomponent.fragments.videoFragmentDirections
+import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+    private lateinit var drawerLayout : DrawerLayout
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
+    private lateinit var listener : NavController.OnDestinationChangedListener
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-       nav.setupWithNavController(nav_host_fragment_container.findNavController())
-        var navController = findNavController(R.id.nav_host_fragment_container)
-        var appConfiguration = AppBarConfiguration(navController.graph)
-        toolbar.setupWithNavController(navController,appConfiguration)
+        navController = findNavController(R.id.nav_host_fragment_container)
+        drawerLayout = findViewById(R.id.drawer)
+        appBarConfiguration = AppBarConfiguration(navController.graph,drawerLayout)
 
-        var actionBarDrawerToggle = ActionBarDrawerToggle(this,drawer,toolbar,0,0)
-        actionBarDrawerToggle.syncState()
-        drawer.addDrawerListener(actionBarDrawerToggle)
-        
+        nav.setupWithNavController(navController)
+
+        toolbar.setupWithNavController(navController,drawerLayout)
+
     }
-}
+
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_container)
+        return navController.navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
+    }
+
+        override fun onBackPressed() {
+            if (drawer.isDrawerOpen(GravityCompat.START))
+                drawer.closeDrawer(GravityCompat.START)
+            else
+                super.onBackPressed()
+        }
+
+    }
+
+
